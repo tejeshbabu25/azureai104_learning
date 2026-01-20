567a36300f5045e05e85e12f0f785e7017c235d2
Tejesh Babu M V <tejeshbabu25@gmail.com>
2026-01-19 20:32:21 -0500

    Added expected output

--- azureailanguageservice_analyze_text.py
+++ azureailanguageservice_analyze_text.py
@@ -15,3 +15,7 @@ mydocuments = [
 response =client.detect_language(documents=mydocuments)
 
 print(response)
+
+# Expected output:
+# PS C:\Tejesh_Development\Python\Dev_Learning\azureai104_learning> & C:/Tejesh_Development/Python/Dev_Learning/azureai104_learning/.venv/Scripts/python.exe c:/Tejesh_Development/Python/Dev_Learning/azureai104_learning/azureailanguageservice_analyze_text.py
+# [DetectLanguageResult(id=0, primary_language=DetectedLanguage(name=English, iso6391_name=en, confidence_score=0.98), warnings=[], statistics=None, is_error=False, kind=LanguageDetection), DetectLanguageResult(id=1, primary_language=DetectedLanguage(name=Spanish, iso6391_name=es, confidence_score=1.0), warnings=[], statistics=None, is_error=False, kind=LanguageDetection)]
